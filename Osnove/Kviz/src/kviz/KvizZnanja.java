71b731c00e88763ad760f0099f3ad2b95fbf7360
Dejan Micic <a01549172@unet.univie.ac.at>
2020-07-17 17:45:11 +0200

    dodata pitanja

--- Osnove/Kviz/src/kviz/KvizZnanja.java
+++ Osnove/Kviz/src/kviz/KvizZnanja.java
@@ -8,7 +8,7 @@ public class KvizZnanja {
 
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
-		int brojPokusaja = 1;
+		int brojPokusaja = 0;
 		System.out.println("Dobrodosao u kviz znanja!");
 		int i = -1;
 		while(i != 0) {
@@ -17,13 +17,13 @@ public class KvizZnanja {
 			System.out.println("2. System.out.print();");
 			System.out.println("3. System.println();");
 			i = input.nextInt();
-			System.out.println("broj pokusaja je : " + brojPokusaja);
-			if(brojPokusaja > 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA");
+			if(brojPokusaja > 0 ) System.out.println("broj pokusaja je : " + brojPokusaja);
+			if(brojPokusaja >= 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA");
 			pitanjeDva();
 			}else
 			switch (i) {
 				case 1:
-					System.out.println("Tacan odg");
+					System.out.println("TACAN ODGOVOR!");
 					brojTacnihOdgovora++;
 					i = 0;
 					pitanjeDva();
@@ -51,8 +51,8 @@ public class KvizZnanja {
 			System.out.println("2. System.out.print();");
 			System.out.println("3. System.println();");
 			i = input.nextInt();
-			System.out.println("broj pokusaja je : " + brojPokusaja);
-			if(brojPokusaja > 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA");
+			if(brojPokusaja > 0 ) System.out.println("broj pokusaja je : " + brojPokusaja);
+			if(brojPokusaja >= 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA");
 			pitanjeTri();
 			}else
 			switch (i) {
@@ -61,7 +61,7 @@ public class KvizZnanja {
 				brojPokusaja++;
 				break;
 			case 2: 
-				System.out.println("Tacan odg!");
+				System.out.println("TACAN ODGOVOR!");
 				brojTacnihOdgovora++;
 				i = 0;
 				pitanjeTri();
@@ -85,20 +85,50 @@ public class KvizZnanja {
 			System.out.println("2. int i = {2,3,4,1,4}; double d = 2; int[] array = {2.4,2.1,5.2};");
 			System.out.println("3. int i = 2.3; double d = 2.3; int[] array = {2,3,1,2,2};");
 			i = input.nextInt();
-			System.out.println("broj pokusaja je : " + brojPokusaja);
-			if(brojPokusaja > 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA - BROJ TACNIH ODG JE: " + brojTacnihOdgovora);
+			if(brojPokusaja > 0 ) System.out.println("broj pokusaja je : " + brojPokusaja);
+			if(brojPokusaja >= 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA");
 			System.exit(0);
 			}else
 			switch (i) {
 			case 1:
-				System.out.println("Tacan odg!");
+				System.out.println("TACAN ODGOVOR!");
 				brojTacnihOdgovora++;
-				System.out.println("broj tacnih odgovora: " + brojTacnihOdgovora + " od ukupno 3");
-				if(brojTacnihOdgovora == 0 || brojTacnihOdgovora == 1) System.out.println("PONOVI GRADIVO PA PROBAJ OPET !!");
-				else
-				if(brojTacnihOdgovora == 2) System.out.println("Nije lose, nije lose!");
-				else
-				if(brojTacnihOdgovora == 3) System.out.println("SVAKA CAST, MOZEMO NASTAVIT SA UCENJEM!");
+				i = 0;
+				pitanjeCetiri();
+				break;
+			case 2: 
+				System.out.println("Pokusaj ponovo");
+				brojPokusaja++;
+				break;
+			case 3: 
+				System.out.println("Pokusaj ponovo");
+				brojPokusaja++;
+				break;
+			default:
+				System.out.println("pogresan unos");
+				break;
+			}
+		}
+	}
+	private static void pitanjeCetiri() {
+		int i = -1;
+		int brojPokusaja = 1;
+		while(i != 0) {
+			System.out.println("Kojom deklaracijom se nekom programu prikljucuju raspolozive klase Java platforme?");
+			System.out.println("1. import");
+			System.out.println("2. export");
+			System.out.println("3. package");
+			System.out.println("4. module");	
+			i = input.nextInt();
+			if(brojPokusaja > 0 ) System.out.println("broj pokusaja je : " + brojPokusaja);
+			if(brojPokusaja >= 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA");
+			System.exit(0);
+			}else
+			switch (i) {
+			case 1:
+				System.out.println("TACAN ODGOVOR!");
+				brojTacnihOdgovora++;
+				pitanjePet();
 				i = 0;
 				break;
 			case 2: 
@@ -108,6 +138,49 @@ public class KvizZnanja {
 			case 3: 
 				System.out.println("Pokusaj ponovo");
 				brojPokusaja++;
+				break;
+			case 4: 
+				System.out.println("Pokusaj ponovo");
+				brojPokusaja++;
+				break;
+			default:
+				System.out.println("pogresan unos");
+				break;
+			}
+		}
+	}
+	private static void pitanjePet() {
+		int i = -1;
+		int brojPokusaja = 1;
+		while(i != 0) {
+			System.out.println("Koji oblik nije ispravno napisana boolean varijabla?");
+			System.out.println("1. boolean tacno = true;");
+			System.out.println("2. boolean pogresno = false;");
+			System.out.println("3. boolean tacno = 1");	
+			i = input.nextInt();
+			if(brojPokusaja > 0 ) System.out.println("broj pokusaja je : " + brojPokusaja);
+			if(brojPokusaja >= 2) { System.out.println("DOSTIGNUT JE MAX BROJ POKUSAJA - BROJ TACNIH ODG JE: " + brojTacnihOdgovora);
+			System.exit(0);
+			}else
+			switch (i) {
+			case 1:
+				System.out.println("Pokusaj ponovo");
+				brojPokusaja++;
+				break;
+			case 2: 
+				System.out.println("Pokusaj ponovo");
+				brojPokusaja++;
+				break;
+			case 3: 
+				System.out.println("TACAN ODGOVOR!");
+				brojTacnihOdgovora++;
+				System.out.println("broj tacnih odgovora: " + brojTacnihOdgovora + " od ukupno 5");
+				if(brojTacnihOdgovora == 0 || brojTacnihOdgovora == 1) System.out.println("PONOVI GRADIVO PA PROBAJ OPET !!");
+				else
+				if(brojTacnihOdgovora == 2 || brojTacnihOdgovora == 3) System.out.println("Nije lose, nije lose!");
+				else
+				if(brojTacnihOdgovora == 4 || brojTacnihOdgovora == 5) System.out.println("SVAKA CAST, MOZEMO NASTAVIT SA UCENJEM!");
+				i = 0;
 				break;
 			default:
 				System.out.println("pogresan unos");
